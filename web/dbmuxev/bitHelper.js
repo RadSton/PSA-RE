b9282f23855a83c1a8a90d855f16ca2180e2b279
radston12 <radston12@gmail.com>
2024-12-17 20:08:58 +0100

    Bits convertion errors to be fixed, again.

--- web/dbmuxev/bitHelper.js
+++ web/dbmuxev/bitHelper.js
@@ -1,7 +1,8 @@
 // This file contains a little suffering 
 // Originaly wanted to base this on https://github.com/RadSton/PSA-RE/commit/247a8fa632d1fe00c7b4fc055f06930bf77090d0#diff-856037e48e5b11ecbda37a2786352dcf9f0ef3bc8252d676511e91d50eac6724R160-R170
 // but I think only the reversing was "directly" copied
-// it works thank god
+// it works thank god ; Update 2 weeks later: no it didnt, id*** radston12
+
 
 
 /**
@@ -15,8 +16,8 @@ module.exports.convertDBMUXBitsToBigEndianFormat = (bits) => {
 
     if(bits.length == 1) bits = bits + ".0";
 
-    const raw = bits.replaceAll(".", "").split("-").map((x) => Number.parseInt(x)); // List of raw values without dot // f.e. "1.7-1.0" -> [7, 0] or "5.0" -> [32]
-    const parsed = raw.map(convertToBigEndianLocation);
+    let raw = bits.replaceAll(".", "").split("-").map((x) => Number.parseInt(x)); // List of raw values without dot // f.e. "1.7-1.0" -> [7, 0] or "5.0" -> [32]
+    let parsed = raw.map(convertToBigEndianLocation);
 
     let data = {
         length: 1,
@@ -25,6 +26,25 @@ module.exports.convertDBMUXBitsToBigEndianFormat = (bits) => {
 
     if (parsed.length == 1)
         return data;
+
+    // you probably call it bad code / avoiding the right solution but I call this a fix since it works!
+    // I dont wanna redo all the complicated formulars just to work for bits above 8 bits of region
+    // The formulars are a pain in the arse and just to make them usefull I spent 3 hours in geogebra and testing javascript soultions
+    // This fix was thought of and made in less than ~ 10 minutes and not 3 hours so I see it as a win!
+    if ((Math.floor(raw[1] / 10) - Math.floor(raw[0] / 10)) > 0) {
+        let diff0 = raw[0] % 10;
+        let diff1 = raw[1] % 10;
+
+        if (diff0 != 0 && ((diff0 == 0 && diff1 == 7) || (diff0 == 7 && diff1 == 0))) {
+            let newRaw = [];
+            
+            newRaw.push(raw[0] - diff0);
+            newRaw.push(raw[1] + diff0);
+
+            raw = newRaw;
+            parsed = raw.map(convertToBigEndianLocation);
+        }
+    }
 
     if (yamlFormatFromBit(raw[0]) < yamlFormatFromBit(raw[1])) {
         data.startBit = parsed[0];
